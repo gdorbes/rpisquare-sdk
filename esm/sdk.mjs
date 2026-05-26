073a330b8d2186e93805d9a52b7e860816b2a365
Guillaume DORBES <guillaume@dorbes.com>
2026-05-26 11:10:27 +0200

    wip...

--- esm/sdk.mjs
+++ esm/sdk.mjs
@@ -27,12 +27,24 @@ const ROM = {
         200: "ok",
         10: "Invalid email address",
         11: "Invalid token",
-        12: "Authentication error"
+        12: "Socket authentication error",
+        13: "Agent loading error"
     }
 }
 let ram = {
     socket: {},      // socket data
     ops: {},         // operation log
+    agents: [],      // agent list
+    opt: {
+        // By default, operations are logged in rpisquare console
+        // Could be overwritten per operation
+        console: true,
+        // By default, operations have no callback function
+        // Could be overwritten per operation
+        callback: false,
+        // Set threshold in ms to filter consecutive monitored events of same type
+        rebounce: 30
+    }
 }
 let fun = {} // Private functions
 export let sdk = {} // Public functions
@@ -114,6 +126,104 @@ fun.connectSocket = (socket) => {
         });
     });
 }
+
+/** --------------------------------------------------------------
+ * @function fun.emitOperation
+ * @description Check command parameters and emit operation object
+ * @param {String} serial   - Target agent serial
+ * @param {Number} line     - GPIO line number
+ * @param {String} cmd      - write, read...
+ * @param {Object} params   - Command dependent. See below
+ * @param {Object} opt      - By default, inherit from constructor.See below
+ *                            Used also to pass monitor callback function for monitoringStart
+ * @return {Number}         - 400, 410, 420 or 200
+ */
+/*
+fun.emitOperation = (serial, line, cmd, params = {}, opt) => {
+
+    opt = {
+        console: ram.opt.console,
+        callback: ram.opt.callback,
+        ...opt
+    }
+
+    let mode
+    switch (cmd) {
+        case "write":
+            mode = "out"
+            break
+        case "read":
+            mode = "in"
+            break
+        case "monitoringStart":
+            mode = "in"
+            break
+        case "monitoringStop":
+            mode = "in"
+            break
+        case "pwmDuty":
+            mode = "pwm"
+            break
+        default: // Used for global agent commands e.g. closeAll, restartAgent
+            mode = "none"
+    }
+
+    const agent = this.agents.find(agent => {
+        return agent.serial === serial
+    })
+
+    // 400: Unknown serial number
+    if (!agent) {
+        warn("gpio operation rejected. Error 0: Unknown serial number")
+        return 400
+    }
+
+    // Line tests when mode is set
+    if (mode !== "none") {
+
+        // 410: Unknown line in serial agent when mode is defined
+        const lineId = Object.keys(agent.gpio).find(id => {
+            return agent.gpio[id].line === line
+        })
+        if (!lineId) {
+            warn("gpio operation rejected. Error 1: Unknown line for defined agent")
+            return 410
+        }
+
+        // 420: Line mode is invalid
+        if (mode !== agent.gpio[lineId].mode) {
+            warn("gpio operation rejected. Error 2: Line mode is not valid:", mode)
+            return 420
+        }
+    }
+    // 200: Define operation. Add it to ops list. Emit it
+    let ope = {
+        id: suid(),
+        command: cmd,
+        serial: serial,
+        line: line,
+        params: params,
+        // Optional acknowledge if callback is defined as a function
+        fbAck: (typeof opt.callback === "function"),
+        // Optional display in rpisquare app console
+        fbCon: opt.console
+    }
+
+    this.ops[ope.id] = {
+        req: ope,
+        reqTime: new Date(),
+        res: false,
+        resTime: false,
+        callback: opt.callback
+    }
+    // Add monitor if required
+    cmd === "monitoringStart" && typeof opt.monitor === "function" ? this.ops[ope.id].monitor = opt.monitor : false
+
+    this.socket.emit("operation", ope)
+    log("ope+", ope)
+    return 200
+}
+*/
 // -------------------------------------------------------------------
 // PUBLIC FUNCTIONS
 /** ------------------------------------------------------------------
@@ -147,10 +257,33 @@ sdk.authenticate = async (email = "", token = "") => {
         log("attempting to connect...")
         await fun.connectSocket(ram.socket)
         log("socket connected")
+
+        log("loading agents...")
+        const res = await fun.api("/client/gpios", {
+            email: email,
+            token: token
+        })
+        if (res.code !== 200)  return fun.response(12, res.msg)
+        ram.agents = res.data
+        log("agents loaded")
+
         return fun.response(200, "")
     } catch (error) {
         return fun.response(12, error.message)
     }
+}
+
+/** --------------------------------------------------------------
+ * @method write
+ * @description Set value of a remote output GPIO
+ * @param {String} serial
+ * @param {Number} line
+ * @param {Number} value
+ * @param {Object} opt - to overwrite default console and feedback
+ * @return {Number}
+ */
+sdk.write = (serial, line, value, opt = {}) => {
+    return fun.emitOperation(serial, line, "write", {toWrite: value}, opt)
 }
 
 // -------------------------------------------------------------------
