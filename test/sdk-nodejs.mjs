03cc61ccf1e3dad2ad5c0983f71f606ade212015
gdorbes <163124016+gdorbes@users.noreply.github.com>
2026-05-26 14:08:06 +0200

    Delete test directory

--- test/sdk-nodejs.mjs
+++
@@ -1,9 +0,0 @@
-// -------------------------------------------------------------------
-// RPISQUARE SDK - TEST FOR NODEJS
-// -------------------------------------------------------------------
-import {sdk} from "../esm/sdk.mjs"
-
-const started = await sdk.authenticate("guillaume@dorbes.com", "00790033007500350041007300530062")
-sdk.log("agent list:", started.data)
-
-
